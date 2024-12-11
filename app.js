3fe46e59f22e9f41e56e8094ca472872db7d1d8d
aagii <altangerelmaakhuu60@gmail.com>
2024-12-11 12:45:15 +0800

    Sanhuugiin moduliin kodiig dichih

--- app.js
+++ app.js
@@ -25,14 +25,54 @@ var uiController = (function() {
   })();
   // Санхүүгийн модуль
   var financeController = (function() {
-
+    // Орлого хадгалах обьект
+    var Income = function(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    };
+    // Зарлага хадгалах обьект
+    var Expense = function(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    };
+    // Бүх өгөгдлүүдийг хадгалах
+    var data = {
+      items: {
+        inc: [],
+        exp: []
+      },
+      totals: {
+        inc: 0,
+        exp: 0
+      }
+    };
+    return {
+      addItem: function(type, desc, val) {
+        var item, id;
+        if(data.items[type].length === 0) id = 1;
+        else {
+          id = data.items[type][data.items[type].length - 1].id + 1;
+        }
+        if(type === "inc") {
+          item = new Income(id, desc, val);
+        }else {
+          item = new Expense(id, desc, val);
+        }
+        data.items[type].push(item);
+      },
+      seeData: function() {
+        return data;
+      }
+    };
   })();
   // Холбогч модуль
   var appController = (function(uiController, financeController) {
    
     var ctrlAddItem = function() {
       // 1. Оруулсан өгөгдлийг дэлгэцнээс олж авах
-      console.log(uiController.getInput());
+      var input = uiController.getInput();
       // 2. Олж авсан өгөгдлүүдээ Санхүүгийн модульд дамжуулж тэнд хадгалах
       // 3. Олж авсан өгөгдлүүдээ тохирох хэсэгт гаргах
       // 4.Төсвийг тооцоолох
@@ -45,8 +85,8 @@ var uiController = (function() {
         ctrlAddItem();
     });
     // enter товч дарах эвент листнер
-    document.addEventListener("keypress", function() {
-      if(Event.keyCode === 13 || Event.which === 13) {
+    document.addEventListener("keypress", function(Event) {
+      if(Event.keyCode === 13) {
         ctrlAddItem();
       }
       });
